bceee0388da8396ee6c981a25b136a6400de22fa
thong1904 <pphhthong1904@gmail.com>
2026-01-18 16:40:42 +0700

    18/1

---
+++ Assets/Code/Manager/CursorManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    void Start()
+    {
+        LockCursor();
+    }
+
+    public void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
